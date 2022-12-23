2bd9fa93e457f499c473f2bbde68dd7eb101bcc8
Brian Samoylov <fender355@icloud.com>
2022-12-23 14:59:35 +0800

    Equation optimized for visualized code

--- workpay.py
+++ workpay.py
@@ -3,13 +3,13 @@ masih = True
 while masih == True: 
     jam = float(input("Nyatakan bilangan jam anda bekerja lebih masa: "))
     
-    bayar=6.5*jam
+    bayar = 6.5 * jam
 
     print(f'Jumlah bayaran yang diperolehi untuk {jam} jam bekerja ialah RM {bayar}.')
 
     guna = int(input("\nAdakah anda masih ingin menggunakan atur cara ini? (1. Ya atau 2. Tidak): "))
 
-    if guna==1:
+    if guna == 1:
         masih
     else:
         masih = False
