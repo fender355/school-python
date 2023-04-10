a505f1a9ffd57f277d5c5e553b6807bac3f22099
Brian Samoylov <fender355@icloud.com>
2023-04-10 18:29:34 +0800

    optimise

--- snakegame.py
+++ snakegame.py
@@ -1,7 +1,7 @@
 from winreg import DisableReflectionKey
 import pygame, sys, time, random
 
-speed=15
+speed=10
 
 #windows size
 
@@ -11,13 +11,13 @@ frame_size_y=1080
 check_errors=pygame.init()
 
 if(check_errors[1] > 0):
-    print("錯誤" + check_errors[1])
+    print("Error!" + check_errors[1])
 else:
-    print("遊戲初始化完畢")
+    print("Game initialise successfully")
 
 #initialize game window
 
-pygame.display.set_caption("貪吃蛇")
+pygame.display.set_caption("Snake Game")
 game_window=pygame.display.set_mode((frame_size_x,frame_size_y))
 
 #colors
