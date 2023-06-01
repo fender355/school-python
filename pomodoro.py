ca24a02502630e113c471b82212ae6ad191a6de1
Brian Samoylov <fender355@icloud.com>
2023-06-01 15:18:16 +0800

    aaa

---
+++ pomodoro.py
@@ -0,0 +1,42 @@
+import time
+import os
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def pomodoro_timer(total_pomodoros):
+    study_time = 1 * 2  # 1 minutes
+    short_break_time = 1 * 2  # 5 minutes
+    pomodoro_count = 0
+
+    while pomodoro_count < total_pomodoros:
+        # Study time
+        clear_screen()
+        print(f"Pomodoro {pomodoro_count + 1}/{total_pomodoros}")
+        print("Study Time!")
+        countdown(study_time)
+
+        # Short break
+        clear_screen()
+        print("Short Break!")
+        countdown(short_break_time)
+
+        pomodoro_count += 1
+
+    clear_screen()
+    print("Congratulations! You have completed all the pomodoros.")
+
+def countdown(duration):
+    while duration:
+        mins, secs = divmod(duration, 60)
+        timer = f"{mins:02d}:{secs:02d}"
+        print(f"Time remaining: {timer}", end="\r")
+        time.sleep(1)
+        duration -= 1
+
+# Entry point of the program
+if __name__ == "__main__":
+    clear_screen()
+    print("Pomodoro Timer")
+    total_pomodoros = int(input("Enter the total number of pomodoros: "))
+    pomodoro_timer(total_pomodoros)
